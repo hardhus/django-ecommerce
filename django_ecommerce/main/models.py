8583b4a9a098af4c1df4dbee50a3a021d3763c29
hardhus <ahmetcemiltoprakk@gmail.com>
2024-02-26 02:35:35 +0300

    before Chapter 11

--- django_ecommerce/main/models.py
+++ django_ecommerce/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StatusReport(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    when = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200)
+
+class MarketingItem(models.Model):
+    img = models.CharField(max_length=255)
+    heading = models.CharField(max_length=300)
+    caption = models.TextField()
+    button_link = models.URLField(null=True, default="register")
+    button_title = models.CharField(max_length=20, default="View details")
--- django_ecommerce/main/views.py
+++ django_ecommerce/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from main.models import MarketingItem, StatusReport
 from users.models import User
 
 
@@ -6,14 +7,27 @@ from users.models import User
 def index(request):
     uid = request.session.get("user")
     if uid is None:
+        market_items = MarketingItem.objects.all()
         return render(
         request,
         "index.html",
-        {"user": False}
+        {"user": False}|
+        {"marketing_items": market_items},
         )
     else:
+        status = StatusReport.objects.all().order_by("-when")[:20]
         return render(
             request,
             "user.html",
-            {"user": User.get_by_id(uid=uid)}
+            {"user": User.get_by_id(uid=uid)}|
+            {"statuses": status},
         )
+
+def report(request):
+    if request.method == "POST":
+        status = request.POST.get("status", "")
+        if status:
+            uid = request.session.get("user")
+            user = User.get_by_id(uid=uid)
+            StatusReport(user=user, status=status).save()
+        return index(request)
